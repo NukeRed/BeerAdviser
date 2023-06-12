dc2e9a28d2432fd37af0d2de514ee4b7935d1402
AlexanderDerkun <38619024+AlexanderDerkun@users.noreply.github.com>
2023-06-12 12:58:53 +0300

    Add Respond to clicks, write simple logic for spinner

--- app/src/main/java/com/derkun/beeradviser/MainActivity.kt
+++ app/src/main/java/com/derkun/beeradviser/MainActivity.kt
@@ -2,10 +2,43 @@ package com.derkun.beeradviser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val findBeer = findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener {
+            // Code that runs when the button is clicked
+            // Your lambda code goes here
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem
+
+            val beerList = getBeersAdvice(color.toString())
+
+            val beers = beerList.reduce{str, item -> str+'\n'+item}
+            val brands = findViewById<TextView>(R.id.brands)
+
+            brands.text = beers
+        }
+
     }
-}
+        fun getBeersAdvice(color: String): List<String> {
+            return when (color) {
+                "Light" -> listOf("Jail Pale Ale", "Lager Lite")
+                "Amber" -> listOf("Jack Amber", "Red Moose")
+                "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
+                else -> listOf("Gout Stout", "Dark Daniel")
+            }
+        }
+    }
+
+
+
+
+
+
